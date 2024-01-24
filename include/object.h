6a374a27b962641191c58f64190afa89ba5db471
Ruzanna Grigoryan <rugrigor@c2r15s2.42yerevan.am>
2024-01-24 19:30:11 +0400

    all

--- include/object.h
+++ include/object.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   object.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrahovha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rugrigor <rugrigor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 16:29:07 by hrahovha          #+#    #+#             */
-/*   Updated: 2024/01/22 14:10:06 by hrahovha         ###   ########.fr       */
+/*   Updated: 2024/01/24 16:08:17 by rugrigor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,12 @@ typedef struct s_scene
 	float		width;
 	float		hight;
 }			t_scene;
+
+typedef struct s_plane
+{
+	float width;
+	float height;
+}				t_plane;
 
 t_scene		*new_scene(t_camera *cam, t_sphere *sphere);
 t_sphere	*new_sphere(t_vector *center, float diameter);
--- src/raytrace.c
+++ src/raytrace.c
@@ -3,12 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   raytrace.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrahovha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rugrigor <rugrigor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 14:37:10 by hrahovha          #+#    #+#             */
-/*   Updated: 2024/01/22 15:34:00 by hrahovha         ###   ########.fr       */
+/*   Updated: 2024/01/24 16:08:49 by rugrigor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+void	ray_trace(void *mlx, void *window, t_scene *scene)
+{
+	int			mlx_x;
+	int			mlx_y;
+	float		ang_x;
+	float		ang_y;
+	t_vector	*vec;
+	t_plane		*plane;
+
+	mlx_x = 0;
+	mlx_y = 0;
+	plane = get_vplane(scene->width, scene->hight, scene->cams->fov);
+	ang_y = (scene->width / 2);
+	
+	ang_x = (scene->hight / 2)
+}
+
+t_plane	*get_vplane(float width, float height, float fov)
+{
+	t_plane *plane;
+
+	plane = malloc(sizeof(t_plane));
+	if (!plane)
+		err_exit(1);
+	plane->width = 1;
+	plane->height = 1;
+}
